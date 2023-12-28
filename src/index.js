5da2caa42b9158f79a7658a340bc69731c1f54b7
chan-0912 <yadavchanchal679@gmail.com>
2023-12-29 00:27:14 +0530

    Set up app using webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const child1 = React.createElement("p", {}, "I am a child1.");
 const child2 = React.createElement("p", {}, "I am a child2.");
 const div = React.createElement("div", { className: "text" }, [child1, child2]);
+//const div = <div>hello name 2</div>
 console.log(div);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
