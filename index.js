9fa9a38b006b3f7283584d1df63ea4192b4d6ddf
chan-0912 <yadavchanchal679@gmail.com>
2023-12-28 22:54:08 +0530

    React via CDN

---
+++ index.js
@@ -0,0 +1,7 @@
+const child1 = React.createElement("p", {}, "I am a child1.");
+const child2 = React.createElement("p", {}, "I am a child2.");
+const div = React.createElement("div", { className: "text" }, [child1, child2]);
+console.log(div);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(div);
