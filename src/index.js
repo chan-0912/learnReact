47ce35d2c169fe855e5a2c7f2f838b8038785919
chan-0912 <yadavchanchal679@gmail.com>
2024-02-28 03:23:43 +0530

    Module 4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const child1 = React.createElement("p", {}, "I am a child1.");
-const child2 = React.createElement("p", {}, "I am a child2.");
-const div = React.createElement("div", { className: "text" }, [child1, child2]);
-//const div = <div>hello name 2</div>
-console.log(div);
+const isMorning = true;
+const morningText = "good morning user";
+const afternoonText = "afternoon user";
+
+const handleClick=(state)=>{
+    console.log("I was clicked",state);
+}
+
+
+const morningElement = <div>{morningText} <span onClick={()=>handleClick(morningText)}>it is 8am</span></div>;
+const afternoonElement = <div>{afternoonText} it is 6pm</div>;
+const greetingElement = isMorning ? morningElement : afternoonElement;
+
+const GreetingComponent = ()=><>{greetingElement}I am inside component</>;
+
+console.log(GreetingComponent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(div);
+root.render(<GreetingComponent/>);
